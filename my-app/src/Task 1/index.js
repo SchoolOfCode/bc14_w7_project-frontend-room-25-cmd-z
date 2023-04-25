896e0896ece1ca4764d8b6ef691cbb411bb7e654
stevenlegere <stevenmarklegere@gmail.com>
2023-04-25 15:04:40 +0100

    added index.js to Task 1 folder

---
+++ my-app/src/Task 1/index.js
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import './App.css';
+import React from 'react';
+import HomeButton from './Task 1/Components/Home Button';
+
+
+function App() {
+  return (
+    <div className="App">
+     <HomeButton />
+    </div>
+)
+}
+
+export default App;

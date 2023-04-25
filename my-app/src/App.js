c82b6f77df7fcf9176db8ec977038822837b7246
h4rsham <harsham.latif@outlook.com>
2023-04-25 14:47:56 +0100

    Created a non functional button that shows on page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
+import React from 'react';
+import HomeButton from './Task 1/Components/Home Button';
+
 
 function App() {
   return (
     <div className="App">
-     
+     <HomeButton />
     </div>
   );
 }
--- my-app/src/Task 1/Components/Home Button/index.js
+++ my-app/src/Task 1/Components/Home Button/index.js
@@ -1,14 +1,11 @@
 // create a button that will take the user to the home page
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './index.css';
 
 const HomeButton = () => {
     return (
         <div className="home-button">
-        <Link to="../../../app.js">
             <button className="home-button">Home</button>
-        </Link>
         </div>
     );
     };

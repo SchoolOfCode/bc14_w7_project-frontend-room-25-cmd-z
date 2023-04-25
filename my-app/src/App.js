93fe6ed3b2687b691c21f77e0e22e9d7ce05f2a5
stevenlegere <stevenmarklegere@gmail.com>
2023-04-25 14:41:56 +0100

    adding button

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,15 @@
-
+import { useState } from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HomeButton } from './Components/Home Button';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     
+     <HomeButton />
     </div>
-  );
+)
 }
 
 export default App;
--- my-app/src/Task 1/Components/Home Button/index.js
+++ my-app/src/Task 1/Components/Home Button/index.js
@@ -1,9 +1,10 @@
 // create a button that will take the user to the home page
 import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './index.css';
 
-const HomeButton = () => {
+function HomeButton () {
     return (
         <div className="home-button">
         <Link to="../../../app.js">

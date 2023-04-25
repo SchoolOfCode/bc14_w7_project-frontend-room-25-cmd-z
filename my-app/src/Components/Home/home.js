af89f1f5a4c6a5cac0e4e65bb8e4a1c32345dcd1
olgaKhristo <olga84h@gmail.com>
2023-04-25 16:27:29 +0100

    home and app

--- my-app/src/Components/Home/home.js
+++ my-app/src/Components/Home/home.js
@@ -4,19 +4,25 @@
  // we need a container which has an image, paragraph with small text, a button that takes us to a next and back page//
 
 import React from 'react';
-
+import "./home.css";
 function HomePage() {
   return (
-    <div>
-     <img src={process.env.PUBLIC_URL + '/pexels-markus-spiske-2004161.jpg'} alt="" />
-      <h1>JavaScript</h1>
+    <div className='container'>
+  
+    <button>cmd-z</button>
+    <h2>JavaScript for beginers</h2>
+     <img src={process.env.PUBLIC_URL + '/pexels-markus-spiske-2004161.jpg'} alt="" style={{ width: "400px", height: "auto" }}/>
+     <br></br>
+     <button>Start Learning JS now</button>
       <p>JavaScript is the world's most popular programming language.<p>
 
         <p>This tutorial will teach you JavaScript from basic to advanced.</p>
 
      </p>JavaScript is the programming language of the Web. JavaScript is easy to learn.</p>
+     
+     
 
-      <button>Start Learning JavaScript now</button>
+      
     </div>
   );
 }

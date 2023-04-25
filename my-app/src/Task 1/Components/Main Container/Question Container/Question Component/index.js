7684ef86dfacc0a8f2846e120e91f21de1895133
stevenlegere <stevenmarklegere@gmail.com>
2023-04-25 15:55:43 +0100

    Testing QuestionComponent

---
+++ my-app/src/Task 1/Components/Main Container/Question Container/Question Component/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function QuestionComponent () {
+    return (
+        <div>
+            <h1>Question Component</h1>
+        </div>
+    );
+}
+
+export default QuestionComponent;
+
+export function H1Test ()
+
+    { return <h1>Question Component</h1>
+}
--- my-app/src/Task 1/index.js
+++ my-app/src/Task 1/index.js
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 import './App.css';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import HomeButton from './Task 1/Components/Home Button';
+import QuestionComponent from './Components/Main Container/Question Container/Question Component/index.js';
+import { H1Test } from './Components/Main Container/Question Container/Question Component/index.js';
+
+ReactDOM.render(<App />, document.getElementById('root'));  //this is the root element in the index.html file
 
 
 function App() {
   return (
     <div className="App">
+    {/* import the question component here */}
+    <QuestionComponent /> {/*this is the question component*/}
      <HomeButton />
+     <H1Test />
     </div>
 )
 }

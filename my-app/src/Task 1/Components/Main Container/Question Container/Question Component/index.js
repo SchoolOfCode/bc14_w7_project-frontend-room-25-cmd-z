bebcde000609a8474b34113637fbcccbbea6eeb9
h4rsham <harsham.latif@outlook.com>
2023-04-26 10:56:58 +0100

    Slight visual changes made

--- my-app/src/Task 1/Components/Main Container/Question Container/Question Component/index.js
+++ my-app/src/Task 1/Components/Main Container/Question Container/Question Component/index.js
@@ -7,8 +7,10 @@ function QuestionComponent () {
             <h1>Question Component</h1>
             <h4> Intro to Variables</h4>
             <p>JavaScript, a "var" is a keyword used to declare a variable, which is a container for storing data. When you declare a variable using the "var" keyword, you are essentially creating a named placeholder in memory that can hold a value.
-The basic syntax for declaring a variable using the "var" keyword is as follows:
-var variableName;</p>
+            The basic syntax for declaring a variable using the "var" keyword is as follows:
+            var variableName;</p>
+            <h5>True/False Statement:</h5>
+            <p> When naming variables in JavaScript, it’s recommended to use reserved keywords as variable names. </p>
         </div>
     );
 }
--- my-app/src/Task 1/Components/Main Container/TrueFalse/TrueFalse.js
+++ my-app/src/Task 1/Components/Main Container/TrueFalse/TrueFalse.js
@@ -2,17 +2,17 @@ import React from 'react';
 import { FormControl, FormControlLabel, RadioGroup, Radio } from '@mui/material';
 
 function TrueFalseQuestion(props) {
-    const [value, setValue] = React.useState('');
+    const [value, setValue] = React.useState(''); // this is the state of the radio button
   
-    const handleChange = (event) => {
-      setValue(event.target.value);
+    const handleChange = (event) => { // this is the function that will be called when the radio button is clicked
+      setValue(event.target.value); // this will set the value of the radio button to the value of the radio button that was clicked
     };
   
     return (
-      <FormControl component="fieldset">
-        <RadioGroup aria-label="truefalse" name="truefalse" value={value} onChange={handleChange}>
-          <FormControlLabel value="true" control={<Radio />} label="True" />
-          <FormControlLabel value="false" control={<Radio />} label="False" />
+      <FormControl component="fieldset"> 
+        <RadioGroup aria-label="truefalse" name="truefalse" value={value} onChange={handleChange}> {/* this is the radio group that will contain the radio buttons */}
+          <FormControlLabel value="true" control={<Radio />} label="True" /> {/* this is the true radio button */}
+          <FormControlLabel value="false" control={<Radio />} label="False" /> {/* this is the false radio button */}
         </RadioGroup>
       </FormControl>
     );

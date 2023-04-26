bb5faeb7f421e0b468b413c9926fde979488d2a2
stevenlegere <stevenmarklegere@gmail.com>
2023-04-26 14:54:10 +0100

    Task 2 almost complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,28 @@
-import { useState } from 'react';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomeButton from './Task 2/Components/Home Button';
+import QuestionComponent from './Task 2/Components/Main Container/Question Container/Question Component/index.js';
+import TitleComponent from './Task 2/Components/Main Container/Title Component';
+import MultipleChoiceQuestion from './Task 2/Components/Main Container/Multiple Choice/index';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
-  );
+    <div className="container">
+    <HomeButton />
+    <TitleComponent />
+    <QuestionComponent />
+    <MultipleChoiceQuestion />
+    <br></br>
+    </div> 
+)
 }
 
 export default App;
+
+
+ReactDOM.render(<App />, document.getElementById('root'));  //this is the root element in the index.html file
---
+++ my-app/src/Task 2/Components/Main Container/Title Component/index.js
@@ -0,0 +1,12 @@
+// create a title component
+import React from 'react';
+
+function TitleComponent () {
+    return (
+        <div>
+            <h1>Task #2</h1>
+        </div>
+    );
+}
+
+export default TitleComponent;

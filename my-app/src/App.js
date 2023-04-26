10d9a51c835f2f4ef01aaa4471f17ff31bcdab3a
danayet <danayetgezai@gmail.com>
2023-04-26 13:36:02 +0100

    improve style of homepage

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 //import Home from './Components/Home';
 import HomePage from './Components/Home/home';
+import NavBar from './Components/Nav-Bar/Nav';
+import ContactPage from './Components/contactPage/contactPage';
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+      <div>
+        <HomePage />
+        <NavBar/>
+        <ContactPage/>
+
+      </div>
+     
+     
+
   );
 }
 
--- my-app/src/Components/Home/home.js
+++ my-app/src/Components/Home/home.js
@@ -1,24 +1,25 @@
-
-
- // we need a home button (logo) at the top of the page//
+// we need a home button (logo) at the top of the page//
  // we need a container which has an image, paragraph with small text, a button that takes us to a next and back page//
 
-import React from 'react';
-import "./home.css";
+import React from 'react'; 
+import "./home.css"; // linked to css//
+
+// This function represents the home page of the website
 function HomePage() {
   return (
     <div className='container'>
-  
-    <button>cmd-z</button>
-    <h2>JavaScript for beginers</h2>
+
+    <h2>JavaScript For Beginers</h2>
      <img src={process.env.PUBLIC_URL + '/pexels-markus-spiske-2004161.jpg'} alt="" style={{ width: "400px", height: "auto" }}/>
      <br></br>
      <button>Start Learning JS now</button>
-      <p>JavaScript is the world's most popular programming language.<p>
+      <p>/ => + - “”</p>
+     <p>JavaScript is the world's most popular programming language.</p>
 
-        <p>This tutorial will teach you JavaScript from basic to advanced.</p>
+<p>This tutorial will teach you JavaScript from basic to advanced.</p>
 
-     </p>JavaScript is the programming language of the Web. JavaScript is easy to learn.</p>
+<p>JavaScript is the programming language of the Web. JavaScript is easy to learn.</p>
+
      
      
 
@@ -28,3 +29,4 @@ function HomePage() {
 }
 
 export default HomePage;
+

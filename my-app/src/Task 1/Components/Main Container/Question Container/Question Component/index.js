4f044572c922017431782c7c5cfe46a4f1aa28b7
stevenlegere <stevenmarklegere@gmail.com>
2023-04-26 11:56:38 +0100

    Added more description

--- my-app/src/Task 1/Components/Main Container/Question Container/Question Component/index.js
+++ my-app/src/Task 1/Components/Main Container/Question Container/Question Component/index.js
@@ -9,6 +9,12 @@ function QuestionComponent () {
             <p>JavaScript, a "var" is a keyword used to declare a variable, which is a container for storing data. When you declare a variable using the "var" keyword, you are essentially creating a named placeholder in memory that can hold a value.
             The basic syntax for declaring a variable using the "var" keyword is as follows:
             var variableName;</p>
+            <br></br>
+            <p>When it comes to naming variables in JavaScript, there are some important principles to keep in mind. These principles can help you create clear and meaningful variable names that are easy to understand and use in your code.</p>
+            <br></br>
+            <p>Use descriptive names: Variable names should be descriptive and indicate what kind of data they hold. This makes it easier for other developers to read and understand your code. For example, instead of using a generic name like "var x", use a more descriptive name like "var numberOfCars".</p>
+            <br></br>
+            <p>Naming variables in JavaScript is an important part of writing clean, readable code. By following these principles, you can create variable names that are meaningful, clear, and easy to work with.</p>
             <h5>True/False Statement:</h5>
             <p> When naming variables in JavaScript, it’s recommended to use reserved keywords as variable names. </p>
         </div>

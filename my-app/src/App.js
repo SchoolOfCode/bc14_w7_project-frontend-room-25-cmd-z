efa8f99075d7c337e4dc1b4882e13f66a277ea16
danayet <danayetgezai@gmail.com>
2023-04-27 16:28:07 +0100

    attempt on Hamburger menu to show

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import ReactDOM from 'react-dom';
 import HomePage from "../src/components/HomePage/Home/home"
 import Task1 from './components/Task1/index';
 import Task2 from './components/Task2/task2';
+import HamburgerMenu from './components/HomePage/HamburgerMenu/index'
 
 // hello, we did it Olga shearing screen
 
@@ -14,11 +15,14 @@ function App() {
     {/* <HomePage/>
     <Task1/>
     <Task2/>
+
    */}
+   {/* <HamburgerMenu/> */}
     <Routes>
         <Route path="/" element={ <HomePage/> } />
         <Route path="Task1" element={ <Task1/> } />
         <Route path="Task2" element={ <Task2/> } />
+        <Route path = "HamburgerMenu" element={<HamburgerMenu/>} />
       </Routes>
 
 
--- my-app/src/components/HomePage/Home/home.js
+++ my-app/src/components/HomePage/Home/home.js
@@ -1,35 +1,37 @@
-// we need a home button (logo) at the top of the page//
- // we need a container which has an image, paragraph with small text, a button that takes us to a next and back page//
-import { Link } from "react-router-dom";
-import React from 'react'; 
-import "./home.css"; // linked to css//
+  // we need a home button (logo) at the top of the page//
+  // we need a container which has an image, paragraph with small text, a button that takes us to a next and back page//
+  import { Link } from "react-router-dom";
+  import React from 'react'; 
+  import "./home.css"; // linked to css//
+  // import Hamburger from "../HamburgerMenu";
 
-// This function represents the home page of the website
-function HomePage() {
-  return (
-    <div className='container'>
+  // This function represents the home page of the website
+  function HomePage() {
+    return (
+      <div className='container'>
 
-    <h2>JavaScript For Beginners</h2>
-     <img src={process.env.PUBLIC_URL + '/pexels-markus-spiske-2004161.jpg'} alt="" style={{ width: "400px", height: "auto" }}/>
-     <br></br>
-     
-     <Link to="Task1">
-     <button>Start Learning JS now</button>
-     </Link>
-      <p>/ = + - “”</p>
-     <p>JavaScript is the world's most popular programming language.</p>
+      <h2>JavaScript For Beginners</h2>
+      <img src={process.env.PUBLIC_URL + '/pexels-markus-spiske-2004161.jpg'} alt="" style={{ width: "400px", height: "auto" }}/>
+      <br></br>
+      
+      <Link to="Task1">
+      <button>Start Learning JS now</button>
+      </Link>
+        <p>/ = + - “”</p>
+      <p>JavaScript is the world's most popular programming language.</p>
+      {/* <Hamburger/> */}
 
-<p>This tutorial will teach you JavaScript from basic to advanced.</p>
+  <p>This tutorial will teach you JavaScript from basic to advanced.</p> 
 
-<p>JavaScript is the programming language of the Web. JavaScript is easy to learn.</p>
-
-     
-     
+  <p>JavaScript is the programming language of the Web. JavaScript is easy to learn.</p>
 
       
-    </div>
-  );
-}
+      
 
-export default HomePage;
+        
+      </div>
+    );
+  }
+
+  export default HomePage;
 

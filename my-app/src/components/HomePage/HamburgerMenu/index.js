3bb34d51fc5c30965e4d18d49446d9a05e026fa0
olgaKhristo <olga84h@gmail.com>
2023-04-27 15:37:37 +0100

    hamburger menu

---
+++ my-app/src/components/HomePage/HamburgerMenu/index.js
@@ -0,0 +1,24 @@
+
+import React from 'react';
+import './nav.css'
+//import 'schoolCode.png' from '../../../public'; // import your logo image file 
+// Have a nav bar to show about us and learn more, this needs to be a link
+// if we have time have a back and next button
+
+function Hamburger() {
+  // nav bar, with link for about us page, learn more, logo and search box //
+  return (
+    <nav className="hamburger"> 
+      <ul>
+        <li>
+       
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Hamburger;
+
+
+
